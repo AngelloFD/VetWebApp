be1c2ccb8b1ae0d1f16c6a5e67b992c10daf4c78
Angello Falla <fallaangello@gmail.com>
2023-06-16 22:24:53 -0500

    Pagina de inicio hecha. Modelos iniciados. Migraciones

--- src/veterinaria/views.py
+++ src/veterinaria/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
+def home_view(request):
+    return render(request, 'home.html', {})
